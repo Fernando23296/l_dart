8556f3c4c668c3b959761daa5710cbb17edd2dcc
Bruno <fernando23296@gmail.com>
2019-04-21 20:48:50 -0400

    Variables in Dart

---
+++ variables.dart
@@ -0,0 +1,18 @@
+void main(){
+  
+  //Declaracion de variables
+  int numero = 12;
+  double flotante = 23.4;
+  String nombre = "Bruno";
+  bool declaracion = true;
+  dynamic comodin = "hola";
+  List list = [1,2,3,4];
+  
+ 	print(numero);
+  print(flotante);
+  print(nombre);
+  print(declaracion);
+  print(comodin); 
+  print(list);
+  
+}
